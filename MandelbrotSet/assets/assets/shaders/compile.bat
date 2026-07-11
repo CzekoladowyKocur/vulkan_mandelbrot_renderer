d8792aa11e34fd1035e28de0574c05eca7d49128
martin-swe-fs <297412436+martin-swe-fs@users.noreply.github.com>
2026-07-11 21:33:15 +0200

    build: remove redundant asset folder copy

--- MandelbrotSet/assets/assets/shaders/compile.bat
+++
@@ -1,4 +0,0 @@
-C:\VulkanSDK\1.2.170.0\Bin32\glslc.exe vertexShader.vert -o vertexShader.spv
-C:\VulkanSDK\1.2.170.0\Bin32\glslc.exe fragmentShader.frag -o fragmentShader.spv
-C:\VulkanSDK\1.2.170.0\Bin32\glslc.exe computeShader.comp -o computeShader.spv
-pause
