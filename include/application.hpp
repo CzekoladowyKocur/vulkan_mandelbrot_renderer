83311074fb169fc6a55a6fbcbd00d42dbdc4b425
Martin <82412982+CzekoladowyKocur@users.noreply.github.com>
2026-07-13 11:06:30 +0200

    chore: rename application sources and use default member init

--- include/application.hpp
+++ include/application.hpp
@@ -71,40 +71,41 @@ private:
   QueueFamilyIndices GetQueueFamilyIndices(int32_t flags);
 
 private:
-  ERenderMethod m_RenderMethod;
-  bool m_Running;
+  ERenderMethod m_RenderMethod{ERenderMethod::Default};
+  bool m_Running{true};
   std::optional<window> m_window;
   input m_input;
   /* Vulkan API */
   /* Instance (loads the vulkan dll driver) */
-  VkInstance m_Instance;
+  VkInstance m_Instance{VK_NULL_HANDLE};
 
   /* Surface*/
-  VkSurfaceKHR m_Surface;
+  VkSurfaceKHR m_Surface{VK_NULL_HANDLE};
 
   /* Physical Device */
-  VkPhysicalDeviceProperties m_PhysicalDeviceProperties;
-  VkPhysicalDeviceFeatures m_PhysicalDeviceFeatures;
-  mutable VkPhysicalDeviceMemoryProperties m_PhysicalDeviceMemoryProperties;
-  VkPhysicalDevice m_PhysicalDevice;
+  VkPhysicalDeviceProperties m_PhysicalDeviceProperties{};
+  VkPhysicalDeviceFeatures m_PhysicalDeviceFeatures{};
+  mutable VkPhysicalDeviceMemoryProperties m_PhysicalDeviceMemoryProperties{};
+  VkPhysicalDevice m_PhysicalDevice{VK_NULL_HANDLE};
 
-  QueueFamilyIndices m_QueueIndices;
+  QueueFamilyIndices m_QueueIndices{};
 
   /* Logical Device */
-  VkDevice m_LogicalDevice;
-  VkQueue m_GraphicsQueue;
-  VkQueue m_ComputeQueue;
-  VkQueue m_PresentQueue;
-  VkCommandPool m_GraphicsCommandPool;
-  VkCommandPool m_ComputeCommandPool;
+  VkDevice m_LogicalDevice{VK_NULL_HANDLE};
+  VkQueue m_GraphicsQueue{VK_NULL_HANDLE};
+  VkQueue m_ComputeQueue{VK_NULL_HANDLE};
+  VkQueue m_PresentQueue{VK_NULL_HANDLE};
+  VkCommandPool m_GraphicsCommandPool{VK_NULL_HANDLE};
+  VkCommandPool m_ComputeCommandPool{VK_NULL_HANDLE};
 
   /* Swapchain */
-  VkSwapchainKHR m_Swapchain;
+  VkSwapchainKHR m_Swapchain{VK_NULL_HANDLE};
 
-  VkSurfaceCapabilitiesKHR m_SurfaceCapabilities;
-  VkSurfaceFormatKHR m_SurfaceFormat;
-  VkPresentModeKHR m_PresentMode;
-  VkExtent2D m_SwapchainExtent;
+  VkSurfaceCapabilitiesKHR m_SurfaceCapabilities{
+      .currentTransform{VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR}};
+  VkSurfaceFormatKHR m_SurfaceFormat{};
+  VkPresentModeKHR m_PresentMode{};
+  VkExtent2D m_SwapchainExtent{.width{1280}, .height{720}};
 
   /* Swapchain sync */
   struct {
@@ -112,15 +113,15 @@ private:
     std::vector<VkSemaphore> RenderComplete;
   } m_Semaphores;
 
-  uint32_t m_MaxFramesInFlight;
-  uint32_t m_ImageCount;
+  uint32_t m_MaxFramesInFlight{2};
+  uint32_t m_ImageCount{};
 
   std::vector<VkImage> m_SwapchainImages;
   std::vector<VkImageView> m_SwapchainImageViews;
   std::vector<VkFramebuffer> m_SwapchainFramebuffers;
 
   /* Swapchain Compatible Renderpass */
-  VkRenderPass m_SwapchainRenderPass;
+  VkRenderPass m_SwapchainRenderPass{VK_NULL_HANDLE};
 
   /* Pipelines*/
   /* Graphics Pipeline */
@@ -128,34 +129,36 @@ private:
   std::optional<vulkan_buffer> m_IndexBuffer;
   std::optional<vulkan_buffer> m_UBOBuffer;
 
-  VkShaderModule m_VertexShaderModule;
-  VkShaderModule m_FragmentShaderModule;
+  VkShaderModule m_VertexShaderModule{VK_NULL_HANDLE};
+  VkShaderModule m_FragmentShaderModule{VK_NULL_HANDLE};
 
-  VkPipeline m_GraphicsPipeline;
-  VkPipelineLayout m_GraphicsPipelineLayout;
+  VkPipeline m_GraphicsPipeline{VK_NULL_HANDLE};
+  VkPipelineLayout m_GraphicsPipelineLayout{VK_NULL_HANDLE};
 
-  VkDescriptorSetLayout m_GraphicsPipelineUBOBufferDescriptorSetLayout;
-  VkDescriptorSetLayout m_GraphicsPipelineColorPaletteDescriptorSetLayout;
-  VkDescriptorPool m_GraphicsPipelineDescriptorPool;
-  VkDescriptorSet m_GraphicsPipelineUBOBufferDescriptorSet;
-  VkDescriptorSet m_GraphicsPipelineColorPaletteDescriptorSet;
+  VkDescriptorSetLayout m_GraphicsPipelineUBOBufferDescriptorSetLayout{
+      VK_NULL_HANDLE};
+  VkDescriptorSetLayout m_GraphicsPipelineColorPaletteDescriptorSetLayout{
+      VK_NULL_HANDLE};
+  VkDescriptorPool m_GraphicsPipelineDescriptorPool{VK_NULL_HANDLE};
+  VkDescriptorSet m_GraphicsPipelineUBOBufferDescriptorSet{VK_NULL_HANDLE};
+  VkDescriptorSet m_GraphicsPipelineColorPaletteDescriptorSet{VK_NULL_HANDLE};
   std::vector<VkCommandBuffer> m_GraphicsPipelineCommandBuffers;
 
   /* Compute Pipeline */
   std::optional<vulkan_buffer> m_ComputePipelineStorageBuffer;
 
-  VkShaderModule m_ComputeShaderModule;
-  VkDescriptorSetLayout m_ComputePipelineDescriptorSetLayout;
-  VkDescriptorPool m_ComputePipelineDescriptorPool;
-  VkDescriptorSet m_ComputePipelineStorageBufferDescriptorSet;
+  VkShaderModule m_ComputeShaderModule{VK_NULL_HANDLE};
+  VkDescriptorSetLayout m_ComputePipelineDescriptorSetLayout{VK_NULL_HANDLE};
+  VkDescriptorPool m_ComputePipelineDescriptorPool{VK_NULL_HANDLE};
+  VkDescriptorSet m_ComputePipelineStorageBufferDescriptorSet{VK_NULL_HANDLE};
 
-  VkPipeline m_ComputePipeline;
-  VkPipelineLayout m_ComputePipelineLayout;
+  VkPipeline m_ComputePipeline{VK_NULL_HANDLE};
+  VkPipelineLayout m_ComputePipelineLayout{VK_NULL_HANDLE};
 
-  VkCommandBuffer m_ComputePipelineCommandBuffer;
+  VkCommandBuffer m_ComputePipelineCommandBuffer{VK_NULL_HANDLE};
   /* Swapchain synchronization */
-  uint32_t m_ImageIndex;
-  uint32_t m_FrameIndex;
+  uint32_t m_ImageIndex{};
+  uint32_t m_FrameIndex{};
 
   std::vector<VkFence> m_InFlightFences;
   std::vector<VkFence> m_ImagesInFlight;
@@ -165,6 +168,6 @@ private:
 
   /* Debug */
 #ifdef APP_DEBUG
-  VkDebugReportCallbackEXT m_DebugReportCallback;
+  VkDebugReportCallbackEXT m_DebugReportCallback{VK_NULL_HANDLE};
 #endif
 };
